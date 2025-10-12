c406e3409ea0f36b44151771b3c94ad4385d4b49
GiioRL <meoweycupentcmc@gmail.com>
2025-10-12 16:10:31 -0400

    implemented Knight.seesSquare()

--- chess/Knight.java
+++ chess/Knight.java
@@ -14,12 +14,18 @@ public class Knight extends Piece {
         return null;
     }
 
+    public boolean seesSquare(int newRow, int newCol) {
+        int rowDelta = Math.abs(row - newRow);
+        int colDelta = Math.abs(col - newCol);
+        return ((rowDelta <=2 && colDelta <= 2) && (rowDelta + colDelta == 3));
+    }
+
     public ArrayList<Piece> sees() {
         ArrayList<Piece> pieces = new ArrayList<Piece>();
         for (int i = -2; i <= 2; i++) {
             for (int j = -2; j <= 2; j++) {
                 if (Math.abs(i) + Math.abs(j) == 3) {
-                    seePiece(row+i, row+j, pieces);
+                    seePiece(row+i, col+j, pieces);
                 }
             }
         }
